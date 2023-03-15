2ab3dcfebd8ad1e6ba2160d3180e42b5925ac9d4
Aleksandr Sukonko <aleksandr.sukonko@gmail.com>
2023-03-15 20:49:05 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,16 @@
+﻿//This is a comment to my code
+
+//rakendus küsib kasutajalt sisestada tema NIME
+//rakendus tervitab kasutajat nimepidi 
+
+Console.WriteLine("Enter your name:"); //output
+
+//string - on sõne
+//string userName;
+//userName = Console.ReadLine();
+//Console.WriteLine("Hello" + ", " + userName + "!"); //output
+
+string userName = Console.ReadLine(); //input
+
+//string interpolation
+Console.WriteLine($"Hello, {userName}!"); //output
